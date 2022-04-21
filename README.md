9384a92c6012894d0c186672b0e1343d0bfe84f8
eduard <ebargues@ohpen.com>
2022-04-21 13:28:28 +0200

    break: LANZ-2299 introduce tags as mandatory inputs

--- README.md
+++ README.md
@@ -2,30 +2,22 @@
 
 Repository containing Ohpen's Github Action to create or update cloudformation stack with Ohpen standard.
 
-<!-- vscode-markdown-toc -->
 * [code-of-conduct](#code-of-conduct)
-	* [github-action](#github-action)
+  * [github-action](#github-action)
 
-<!-- vscode-markdown-toc-config
-	numbering=false
-	autoSave=false
-	/vscode-markdown-toc-config -->
-<!-- /vscode-markdown-toc -->
-
-
-## <a name='code-of-conduct'></a>code-of-conduct
+## code-of-conduct
 
 Go crazy on the pull requests :) ! The only requirements are:
 
-> - Use [conventional-commits](#check-conventional-commits).
-> - Include [jira-tickets](#check-jira-tickets-commits) in your commits.
-> - Create/Update the documentation of the use case you are creating, improving or fixing. **[Boy scout](https://biratkirat.medium.com/step-8-the-boy-scout-rule-robert-c-martin-uncle-bob-9ac839778385) rules apply**. That means, for example, if you fix an already existing workflow, please include the necessary documentation to help everybody. The rule of thumb is: _leave the place (just a little bit)better than when you came_.
+> * Use [conventional-commits](#check-conventional-commits).
+> * Include [jira-tickets](#check-jira-tickets-commits) in your commits.
+> * Create/Update the documentation of the use case you are creating, improving or fixing. **[Boy scout](https://biratkirat.medium.com/step-8-the-boy-scout-rule-robert-c-martin-uncle-bob-9ac839778385) rules apply**. That means, for example, if you fix an already existing workflow, please include the necessary documentation to help everybody. The rule of thumb is: _leave the place (just a little bit)better than when you came_.
 
-### <a name='github-action'></a>github-action
+### github-action
 
 This action will create or update cloudformation stack in AWS account. The action is build to get json file as cloudformation parameters. For more information about Cloudformation parameters see [docu](https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/parameters-section-structure.html)
 
-On backend action is built as shell script implementing [aws cli](https://docs.aws.amazon.com/cli/latest/reference/cloudformation/index.html#cli-aws-cloudformation) 
+On backend action is built as shell script implementing [aws cli](https://docs.aws.amazon.com/cli/latest/reference/cloudformation/index.html#cli-aws-cloudformation)
 
 example usage:
 
@@ -35,20 +27,26 @@ on:
   push:
     branches: ["main"]
 jobs:
-    build:
-    runs-on: ubuntu-latest
-    steps:
-      - uses: actions/checkout@v3
-       - name: create-or-update-stack
-        uses: ohpensource/create-or-update-cfn-stack-gh-action/action@v0.0.2
-        with:
-          region: ${{ secrets.AWS_REGION }}
-          access-key: ${{ secrets.AWS_ACCESS_KEY_ID }}
-          secret-key: ${{ secrets.AWS_SECRET_ACCESS_KEY }}
-          account: ${{steps.get-properties.outputs.account_id}}
-          role-name: <ROLE_NAME>
-          stack-name: <NAME OF THE STACK TO UPDATE>
-          template-body-path: <PATH TO THE CFN TEMPLATE>
-          cfn-parameters-path: <PATH TO THE PARAMETERS JSON FILE>
-          
+    cfn-deployment:
+      runs-on: ubuntu-latest
+      steps:
+        - uses: actions/checkout@v3
+        - name: create-or-update-stack
+          uses: ohpensource/create-or-update-cfn-stack-gh-action@v1.0.0
+          with:
+            region: "eu-west-1"
+            access-key: "${{ secrets.COR_CICD_AUTOMATION_AWS_ACCESS_KEY_ID }}"
+            secret-key: "${{ secrets.COR_CICD_AUTOMATION_AWS_SECRET_ACCESS_KEY }}"
+            account: "543***"
+            role-name: "admin-role"
+            stack-name: "this-is-a-stack"
+            template-body-path: "cfn/template.yaml"
+            cfn-parameters-path: "cfn/parameters.json"
+            stage: "dev"
+            repository-url: "https://github.com/ohpensource/create-or-update-cfn-stack-gh-action"
+            client: "me"
+            service: "just-for-testing"
+            service-group: "main"
+            team: "team-A"
+            datadog: "false"
 ```
--- create-or-update-stack.sh
+++ create-or-update-stack.sh
@@ -1,5 +1,5 @@
 set -e 
-WORKING_FOLDER=$(pwd)
+working_folder=$(pwd)
 
 log_action() {
     echo "${1^^} ..."
@@ -20,61 +20,97 @@ assume_role() {
     local CREDENTIALS_FILE_NAME="aws-credentials-$(basename "$0").json"
     if [[ ! -f "$CREDENTIALS_FILE_NAME" ]]; then
         local AWS_ACCOUNT_ID=$1
-        local ROLE_NAME=$2
-        local ROLE_ARN="arn:aws:iam::$AWS_ACCOUNT_ID:role/$ROLE_NAME"
+        local role_name=$2
+        local ROLE_ARN="arn:aws:iam::$AWS_ACCOUNT_ID:role/$role_name"
         aws sts assume-role --role-arn $ROLE_ARN --role-session-name github-session > $CREDENTIALS_FILE_NAME
     fi
 
     export AWS_ACCESS_KEY_ID=$(jq -r '.Credentials.AccessKeyId' $CREDENTIALS_FILE_NAME)
     export AWS_SECRET_ACCESS_KEY=$(jq -r '.Credentials.SecretAccessKey' $CREDENTIALS_FILE_NAME)
     export AWS_SESSION_TOKEN=$(jq -r '.Credentials.SessionToken' $CREDENTIALS_FILE_NAME)
-}
+}   
 
 log_action "deploying cloudformation stack"
-REGION=$1
-log_key_value_pair "region" $REGION
-ACCESS_KEY=$2
-log_key_value_pair "access-key" $ACCESS_KEY
-SECRET_KEY=$3
-ACCOUNT_ID=$4
-log_key_value_pair "account-id" $ACCOUNT_ID
-ROLE_NAME=$5
-log_key_value_pair "role-name" $ROLE_NAME
-STACK_NAME=$6
-log_key_value_pair "stack-name" $STACK_NAME
-TEMPLATE_BODY_PATH=$7
-log_key_value_pair "template-body-path" $TEMPLATE_BODY_PATH
-TEMPLATE_BODY="$WORKING_FOLDER/$TEMPLATE_BODY_PATH"
-CFN_PARAMETERS_PATH=$8
-log_key_value_pair "cfn-parameters-path" $CFN_PARAMETERS_PATH
-CFN_PARAMETERS_FILE="$WORKING_FOLDER/$CFN_PARAMETERS_PATH"
 
-set_up_aws_user_credentials $REGION $ACCESS_KEY $SECRET_KEY
-assume_role $ACCOUNT_ID $ROLE_NAME
+while getopts "a:b:c:d:e:f:g:h:i:j:k:l:m:n:o:" flag
+do
+    case "${flag}" in
+       a) region=${OPTARG};;
+       b) access_key=${OPTARG};;
+       c) secret_key=${OPTARG};;
+       d) account_id=${OPTARG};;
+       e) role_name=${OPTARG};;
+       f) stack_name=${OPTARG};;
+       g) template_body_path=${OPTARG};;
+       h) cfn_parameters_path=${OPTARG};;
+       i) stage=${OPTARG};;
+       j) repository_url=${OPTARG};;
+       k) client=${OPTARG};;
+       l) service=${OPTARG};;
+       m) service_group=${OPTARG};;
+       n) team=${OPTARG};;
+       o) datadog=${OPTARG};;
+    esac
+done
 
-STACK=$(aws cloudformation describe-stacks --stack-name $STACK_NAME | jq -r '.Stacks[0]')
-STACK_FILE="stack-file.json"
-if [ -z "$STACK" ]; 
+template_body="$working_folder/$template_body_path"
+cfn_parameters_file="$working_folder/$cfn_parameters_path"
+
+log_key_value_pair "region" $region
+log_key_value_pair "access-key" $access_key
+log_key_value_pair "account-id" $account_id
+log_key_value_pair "role-name" $role_name
+log_key_value_pair "stack-name" $stack_name
+log_key_value_pair "template-body" $template_body
+log_key_value_pair "cfn-parameters-file" $cfn_parameters_file
+log_key_value_pair "stage" $stage
+log_key_value_pair "repository-url" $repository_url
+log_key_value_pair "client" $client
+log_key_value_pair "service" $service
+log_key_value_pair "service-group" $service_group
+log_key_value_pair "team" $team
+log_key_value_pair "datadog" $datadog
+
+set_up_aws_user_credentials $region $access_key $secret_key
+assume_role $account_id $role_name
+
+stack=$(aws cloudformation describe-stacks --stack-name $stack_name | jq -r '.Stacks[0]')
+stack_file="stack-file.json"
+if [ -z "$stack" ]; 
 then
     log_action "creating stack"
     aws cloudformation create-stack \
-        --stack-name $STACK_NAME \
-        --template-body "file://$TEMPLATE_BODY" \
-        --parameters "file://$CFN_PARAMETERS_FILE" \
-        --capabilities "CAPABILITY_NAMED_IAM" >> $STACK_FILE
-    STACK_ARN=$(jq -r '.StackId' $STACK_FILE)
-    log_key_value_pair "stack-arn" $STACK_ARN
+        --tags Key=Stage,Value=$stage \
+               Key=IacRepo,Value=$repository_url \
+               Key=Client,Value=$client \
+               Key=Service,Value=$service \
+               Key=ServiceGroup,Value=$service_group \
+               Key=Team,Value=$team \
+               Key=Datadog,Value=$datadog \
+        --stack-name $stack_name \
+        --template-body "file://$template_body" \
+        --parameters "file://$cfn_parameters_file" \
+        --capabilities "CAPABILITY_NAMED_IAM" >> $stack_file
+    stack_arn=$(jq -r '.StackId' $stack_file)
+    log_key_value_pair "stack-arn" $stack_arn
     aws cloudformation wait stack-create-complete \
-        --stack-name $STACK_ARN
+        --stack-name $stack_arn
 else
     log_action "updating stack"
     aws cloudformation update-stack \
-        --stack-name $STACK_NAME \
-        --template-body "file://$TEMPLATE_BODY" \
-        --parameters "file://$CFN_PARAMETERS_FILE" \
-        --capabilities "CAPABILITY_NAMED_IAM" >> $STACK_FILE
-    STACK_ARN=$(jq -r '.StackId' $STACK_FILE)
-    log_key_value_pair "stack-arn" $STACK_ARN
+        --tags Key=Stage,Value=$stage \
+               Key=IacRepo,Value=$repository_url \
+               Key=Client,Value=$client \
+               Key=Service,Value=$service \
+               Key=ServiceGroup,Value=$service_group \
+               Key=Team,Value=$team \
+               Key=Datadog,Value=$datadog \
+        --stack-name $stack_name \
+        --template-body "file://$template_body" \
+        --parameters "file://$cfn_parameters_file" \
+        --capabilities "CAPABILITY_NAMED_IAM" >> $stack_file
+    stack_arn=$(jq -r '.StackId' $stack_file)
+    log_key_value_pair "stack-arn" $stack_arn
     aws cloudformation wait stack-update-complete \
-        --stack-name $STACK_ARN
+        --stack-name $stack_arn
 fi
